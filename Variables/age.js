7bcd171514b7d532864db01c6d1e17729665edc9
mitchelllantz <mitchell.lantz@icloud.com>
2023-07-18 00:44:37 +0000

    age

---
+++ Variables/age.js
@@ -0,0 +1,6 @@
+let age = 37;
+console.log(`You are ${age} years old.
+            In 10 years, you will be ${age + 10} years old.
+            In 20 years, you will be ${age + 20} years old.
+            In 30 years, you will be ${age + 30} years old.
+            In 40 years, you will be ${age + 40} years old.`);
