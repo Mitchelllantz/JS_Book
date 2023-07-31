fc35d43526b1c741d0e4f2035c1ee0c45e4cbdc5
mitchelllantz <mitchell.lantz@icloud.com>
2023-07-31 19:35:11 +0000

    move

--- Arrays/exercise6.js
+++ Arrays/exercise6.js
@@ -1,9 +1,15 @@
 let arr = ['a', 'abcd', 'abcde', 'abc', 'ab'];
+let oddLengths = (array) => {
+return array.reduce(function(filteredArray, strings){
+  let length = strings.length;
+  if(length % 2 !== 0){
+    filteredArray.push(length);
+  }
+  return filteredArray;
+},[]);
 
-const oddLengths = (arr) => {
-  let lengths = arr.map(length => length.length);
+  }
 
-  return lengths.filter(odd => odd % 2 !== 0);
-}
+
 
 console.log(oddLengths(arr)); // => [1, 5, 3]
